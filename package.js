6d6f790df82ff6feae8c751d36aa40e7a819e90b
rickster444 <r-kortkamp@web.de>
2026-03-16 21:19:13 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "staemme-monitor",
+  "version": "1.0.0",
+  "description": "Heartbeat server for Die Stämme Discord login monitor",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
